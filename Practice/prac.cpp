5cf8d08dd5b288fd2f99abf2637d86775bcdedae
amrita7734 <amrita7734kashyap@gmail.com>
2022-12-23 01:14:02 +0530

    setup

---
+++ Practice/prac.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+    int a,b;
+    cin>>a>>b;
+    cout<<a+b;
+    // cerr<<"time taken: "<<(float)clock()/CLOCKS_PER_SEC<<" secs"<<endl;
+    return 0;
+}
